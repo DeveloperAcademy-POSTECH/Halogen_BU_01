ec8f2a40643a79b5615088fc0aa4b456c294ded6
HanGyeongjun <94854258+HanGyeongjun@users.noreply.github.com>
2022-05-25 20:09:08 +0900

    Create .gitignore, Create project file

---
+++ BeyondUI-Halogen/BeyondUI-Halogen/BeyondUI_HalogenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeyondUI_HalogenApp.swift
+//  BeyondUI-Halogen
+//
+//  Created by 한경준 on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct BeyondUI_HalogenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
